93d34b91ed8bb821b7a49c718c21c0c984da7cd9
Berat Metehan Cakmak <cakmak.metehan58@gmail.com>
2025-12-16 01:10:02 +0300

    Clean up Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 using UserManagementApi.Middleware;
 using UserManagementApi.Repositories;
-using Swashbuckle.AspNetCore.SwaggerGen; // Gerekirse ekle
+using Swashbuckle.AspNetCore.SwaggerGen; 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Servisler
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,22 +12,22 @@ builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
 
 var app = builder.Build();
 
-// 2. Middleware Pipeline (Sıralama Önemli!)
 
-// A. Hata Yakalama (En dışta, her şeyi kapsar)
+
+
 app.UseMiddleware<GlobalExceptionMiddleware>();
 
-// B. Swagger (Geliştirici dostu arayüz)
+
 app.UseSwagger();
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "UserManagementAPI v1");
 });
 
-// C. Kimlik Doğrulama (Auth)
+
 app.UseMiddleware<SimpleAuthMiddleware>();
 
-// D. Loglama (Sadece yetkili istekleri loglar, Auth'dan sonra geldiği için)
+
 app.UseMiddleware<RequestLoggingMiddleware>();
 
 app.MapControllers();
